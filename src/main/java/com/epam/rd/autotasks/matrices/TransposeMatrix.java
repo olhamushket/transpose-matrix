35b87ac3a1e1c5fc415c1cc60ed247197306fca4
Olha Mushket <olhamushket@gmail.com>
2022-07-01 00:15:11 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/matrices/TransposeMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/TransposeMatrix.java
@@ -1,23 +1,38 @@
 package com.epam.rd.autotasks.matrices;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TransposeMatrix {
     public static int[][] transpose(int[][] matrix) {
-
-        //Put your code here
-
-        return null;
+        int [][] trans = new int[matrix[0].length][matrix.length];
+        int n = Math.min(trans.length, trans[0].length);
+        for (int i=0; i<n; i++) {
+            trans[i][i]=matrix[i][i];
+        }
+        for (int i=0; i<trans.length; i++) {
+            for (int j=0; j<trans[0].length; j++) {
+                if (i!=j) {
+                    trans[i][j]=matrix[j][i];
+                }
+            }
+        }
+        return trans;
     }
 
     public static void main(String[] args) {
 
         System.out.println("Test your code here!\n");
 
-        // Get a result of your code
-
-        int[][] matrix = {
-                {1, 2},
-                {7, -13} };
+        int [][] matrix = new int[20][20];
+        Scanner scan = new Scanner(System.in);
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[0].length; j++) {
+                matrix[i][j]= scan.nextInt();
+            }
+        }
+ //        int[][] matrix = {
+//                {1, 2},
+//                {7, -13} };
 
         int[][] result = transpose(matrix);
         System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
